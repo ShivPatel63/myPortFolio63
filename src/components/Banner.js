1ebc6265a51893959d989fcfd030dfcf48a7ef70
ShivPatel63 <shilu6320@gmail.com>
2023-10-16 20:38:12 -0500

    last commit

---
+++ src/components/Banner.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Software Developer", "AI/ML Engineer", "MERN - Full Stack Developer" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hey ! I'm Shiv Patel.`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Developer", "AI/ML Engineer", "MERN - Full Stack Developer" ]'><span className="wrap">{text}</span></span></h1>
+                  {/* /<p>I am creative full stack web Developer! I am enthusiastic and passionate about my work.I have done my masters in computer science.</p> */}
+                  <p>Software Developer with 3+ years of experience spanning industries such as education, supply chain
+                    management, and web development; proficient in software development domains including MERN full-stack and Data Science/Machine learning, with a robust portfolio of projects.
+                    Developer with expertise in programming languages (JavaScript, Python, JAVA), frameworks and libraries
+                    (React, Redux, Express.js, Spring, Spring boot), and cloud services (AWS, MongoDB Atlas), demonstrating a strong foundation in software
+                    development and architecture.
+                  </p>
+                  <button onClick={() => console.log('connect')}><a href="mailto:patel.shiv6320@gmail.com">Let’s Connect <ArrowRightCircle size={25} /></a></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
